fd11566a1fa9c7d0ff113eebec9415d2692cf796
Todd Kaufmann <toddkaufmann+g@gmail.com>
2019-04-25 21:27:16 -0400

    tag with the github repo name and version

--- docker-build-it.sh
+++ docker-build-it.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
+# -eu
 
-TAG=tk/scipy-1226.4
+TAG=toddkaufmann/scipy-notebook-tk
 # why doesn't this work ?
-docker build --tag "$TAG" .
-#  --label $(logfile tk scipy) $*
+docker build \
+       --tag "$TAG":latest \
+       --tag "$TAG":$(cat VERSION) \
+   .
 
-echo 
+STATUS=$?
+
 IMAGE=$(docker images --format '{{.ID}}' | head -1)
+echo
+echo Your image is ready: $IMAGE
+echo $(date) $(cat VERSION) $STATUS $IMAGE >> history
 
-echo "why doesn't this work?"
-docker tag "$IMAGE" "$TAG"
+#VERSION=1
+#echo "why doesn't this work?"
+#docker tag "$IMAGE" "$TAG":"$VERSION"
 # docker tag 8a7 tk/scipy-1226.2
